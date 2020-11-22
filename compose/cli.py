2930cbf586cb8973aa8ca6b33b1478faf285ae42
kolotaev <kolotaev@tut.by>
2020-11-22 21:32:28 +0300

    uncolored output

--- compose/cli.py
+++ compose/cli.py
@@ -2,13 +2,15 @@ import click
 
 from .config import Config
 from .executor import Executor
-from .printing import Printer, ClickEchoWriter
+from .printing import Printer, ClickEchoWriter, PrintWriter
 from .info import version as app_version
 
 
 @click.group()
 def root():
     pass
+    # ctx.ensure_object(dict)
+    # ctx.obj['color'] = color
 
 
 @root.command()
@@ -22,14 +24,18 @@ def version():
 @root.command()
 @click.option('-f', '--file', show_default=True, default='local-compose.yaml', help='Configuration file.')
 @click.option('-b', '--build', is_flag=True, show_default=True, help='Build services before run.')
+@click.option('--color/--no-color', default=True, show_default=True, help='Use colored output?')
 @click.pass_context
-def up(ctx, file, build):
+def up(ctx, file, build, color):
     '''
     Start services
     '''
     conf = Config(file).try_parse()
     processes = []
-    writer = ClickEchoWriter()
+    if color:
+        writer = ClickEchoWriter()
+    else:
+        writer = PrintWriter()
     executor = Executor(printer=Printer(writer))
     services = conf['services']
     import os
