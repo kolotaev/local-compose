317ea06eae068d163fce631d62f7636c8e8cc61b
kolotaev <kolotaev@tut.by>
2020-12-05 17:32:37 +0300

    Add Message tests

---
+++ tests/unit/test_massage.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from compose.printing import Message
+from compose.utils import now
+
+
+def test_message_defaults():
+    m = Message('line', u'hello it is me на русском', 'web1')
+    assert 'line' == m.type
+    assert u'hello it is me на русском' == m.data
+    assert 'web1' == m.name
+    assert m.time <= now()
+    assert m.color is None
+
+
+def test_message_properties():
+    my_time = now()
+    data = '''
+        multi
+        line
+        string
+    '''
+    m = Message('stop', data, 'web1', time=my_time, color='red')
+    assert 'stop' == m.type
+    assert data == m.data
+    assert 'web1' == m.name
+    assert my_time == m.time
+    assert 'red' == m.color
