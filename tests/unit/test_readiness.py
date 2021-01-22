d42cc6764a4eb0c6c5309ccbf880a27725556c1a
kolotaev <kolotaev@tut.by>
2021-01-23 00:52:06 +0300

    Add readiness tests

---
+++ tests/unit/test_readiness.py
@@ -0,0 +1,29 @@
+import pytest
+
+from compose.readiness import Readiness, RetryLogic
+
+
+@pytest.mark.parametrize('config', [
+    ({}),
+    ({
+        'retry': {}
+    }),
+])
+def test_readiness_empty_config_create_defaults(config):
+    r = Readiness(config)
+    assert r.retry is not None
+    assert r.retry.attempts == float('inf')
+    assert r.retry.wait == 5
+
+
+def test_readiness():
+    conf = {
+        'retry': {
+            'wait': 30,
+            'attempts': 50,
+        },
+    }
+    r = Readiness(conf)
+    assert r.retry is not None
+    assert r.retry.attempts == 50
+    assert r.retry.wait == 30
