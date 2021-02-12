e3e6126d05d3fc3ab06a39bcddaf59fa37254225
kolotaev <kolotaev@tut.by>
2021-02-12 17:24:21 +0300

    Add file logging foundation

--- compose/printing.py
+++ compose/printing.py
@@ -6,6 +6,30 @@ import colored
 from .messaging import Line, SYSTEM_LABEL
 
 
+# import logging
+# import time
+# from logging.handlers import TimedRotatingFileHandler
+# #----------------------------------------------------------------------
+# def create_timed_rotating_log(path):
+#     """"""
+#     logger = logging.getLogger("Rotating Log")
+#     logger.setLevel(logging.INFO)
+
+#     handler = TimedRotatingFileHandler(path,
+#                                        when="m",
+#                                        interval=1,
+#                                        backupCount=5)
+#     logger.addHandler(handler)
+
+#     for i in range(6):
+#         logger.info("This is a test!")
+#         time.sleep(75)
+# #----------------------------------------------------------------------
+# if __name__ == "__main__":
+#     log_file = "timed_test.log"
+#     create_timed_rotating_log(log_file)
+
+
 class SimplePrintWriter(object):
     '''
     Basic writer that uses `print` function.
@@ -27,6 +51,7 @@ class ColoredPrintWriter(object):
         '''
         Write a message
         '''
+        # todo - color only
         if color is None or not sys.stdout.isatty():
             print(message)
         else:
