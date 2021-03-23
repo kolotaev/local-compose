bfc47b340d95fcd4743dc563a3a2a3981afc6084
kolotaev <kolotaev@tut.by>
2021-03-23 22:13:14 +0300

    Add basic rotating writer

--- compose/printing.py
+++ compose/printing.py
@@ -1,33 +1,29 @@
 from __future__ import print_function
 import sys
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 import colored
 
 from .messaging import Line, SYSTEM_LABEL
 
 
-# import logging
-# import time
-# from logging.handlers import TimedRotatingFileHandler
-# #----------------------------------------------------------------------
-# def create_timed_rotating_log(path):
-#     """"""
-#     logger = logging.getLogger("Rotating Log")
-#     logger.setLevel(logging.INFO)
+class RotatingFileWriter(object):
+    '''
+    Writer that writes data in the log files of a specified size, that are rotating on size exceed.
+    '''
+    def __init__(self):
+        path = 'some-path-to-log-file.log'
+        self.logger = logging.getLogger('Rotating Log')
+        self.logger.setLevel(logging.INFO)
+        handler = TimedRotatingFileHandler(path, when="m", interval=1, backupCount=5)
+        self.logger.addHandler(handler)
 
-#     handler = TimedRotatingFileHandler(path,
-#                                        when="m",
-#                                        interval=1,
-#                                        backupCount=5)
-#     logger.addHandler(handler)
-
-#     for i in range(6):
-#         logger.info("This is a test!")
-#         time.sleep(75)
-# #----------------------------------------------------------------------
-# if __name__ == "__main__":
-#     log_file = "timed_test.log"
-#     create_timed_rotating_log(log_file)
+    def write(self, message, color=None):
+        '''
+        Write a message
+        '''
+        self.logger.info(message)
 
 
 class SimplePrintWriter(object):
